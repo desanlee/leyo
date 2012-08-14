0ef3b1ea70b156792530903ba8ac9927be8b5c96
desanlee <desanlee@gmail.com>
2012-08-14 18:58:52 +0800

    the first edition

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leyo::Application.config.secret_token = '42a54e48e6a8957cc8cc95fb6a9c76f704c50e1b5636827975f44b22ce22a0fd820d27b1ea5e345ef613e638bd89e7d459750e9a0ee189ed3d0d29d746bd753b'
